fcc045dcb36b4c149e0e7be0048762fb4b8c033a
Teddy Paz Muñoz <pazteddy@gmail.com>
2022-07-18 23:08:04 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,25 @@
+Rails.application.routes.draw do
+  root "games#index"
+
+  resources :critics
+  resources :involved_companies
+  resources :games do
+    resources :critics
+
+    # /games/:id/add_genre
+    post "add_genre", on: :member
+    # /games/:id/remove_genre
+    delete "remove_genre", on: :member
+
+    # /games/:id/add_platform
+    post "add_platform", on: :member
+    # /games/:id/remove_platform
+    delete "remove_platform", on: :member
+  end
+  resources :platforms
+  resources :genres
+  resources :companies do
+    resources :critics
+  end
+  resources :users
+end
---
+++ db/migrate/20220124043443_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.date :birth_date
+      t.string :first_name
+      t.string :last_name
+      t.integer :critics_count, default: 0
+
+      t.timestamps
+    end
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+  end
+end
