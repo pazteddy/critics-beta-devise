173d3f46d4302c99f079a19f11a27415abad5b3b
Teddy <pazteddy@gmail.com>
2022-07-20 00:06:20 -0400

    Add omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
   # Validations
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 16 }
@@ -12,4 +12,14 @@ class User < ApplicationRecord
 
   # Association
   has_many :critics, dependent: :destroy
+
+  def self.from_omniauth(auth_hash)
+    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
+      user.username = auth_hash.info.nickname
+      user.email = auth_hash.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
